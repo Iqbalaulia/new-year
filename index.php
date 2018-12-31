8449aa6821c6cd627b3744f3d489caf929ceff76
iqbalaulia <miqbalar16@gmail.com>
2018-12-31 22:25:19 +0700

    new

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="index.css">
+    <title>Tahun Baru</title>
+</head>
+test
+<body>
+    <div class="feliz">Selamat Tahun Baru !</div>
+    <div class="ano_novo">
+        <span>201</span>
+        <span class="seis">8</span>
+        <span class="sete">9</span>
+        <div class="balao"></div>
+    </div>
+    <div class="fogos">
+        <div class="f1">
+            <span><i></i></span>
+            <span><i></i></span>
+            <span><i></i></span>
+        </div>
+        <div class="f2">
+            <span><i></i></span>
+            <span><i></i></span>
+            <span><i></i></span>
+        </div>
+        <div class="f3">
+            <span><i></i></span>
+            <span><i></i></span>
+            <span><i></i></span>
+        </div>
+        <div class="f4">
+            <span><i></i></span>
+            <span><i></i></span>
+            <span><i></i></span>
+        </div>
+    </div>
+    <a href="https://github.com/Iqbalaulia" class="author">M. IQBAL AULIA RAFI'</a>
+</body>
+
+</html>
